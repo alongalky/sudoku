ab9b36dd19997c726c3e00674264c99535b93c06
Alon Gal <alongalky@gmail.com>
2019-03-03 01:31:45 +0200

    Backend boilerplate

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+extern crate rusqlite;
+
+use rusqlite::{Connection, Result, NO_PARAMS};
+
+#[derive(Debug)]
+struct Game {
+    name: String
 }
+
+#[get("/<game>")]
+fn index(game: String) -> Result<String> {
+    let conn = Connection::open("games.db")?;
+
+    let mut stmt = conn.prepare("SELECT name from games WHERE name == $1")?;
+
+    let games = stmt.query_map(&[&game], |row| {
+        Game {
+            name: row.get(0),
+        }
+    })?;
+
+    if (games.count() > 0) {
+        return Ok(String::from("game exists!"));
+    }
+
+    conn.execute(
+        "INSERT INTO games (name) values ($1)",
+        &[&game],
+    )?;
+
+    return Ok(String::from("Created game"));
+}
+
+fn init_db() -> Result<()> {
+    let mut conn = Connection::open("games.db")?;
+
+    conn.execute(
+        "create table if not exists games (
+             id integer primary key,
+             name text not null unique
+         )",
+        NO_PARAMS,
+    )?;
+
+    Ok(())
+}
+
+fn main() {
+    init_db();
+
+    rocket::ignite()
+        .mount("/", routes![index])
+        .launch();
+}
+
