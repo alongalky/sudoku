fd15434e6ef9dcaf53f6113e652b575314e646c0
Alon Gal <alongalky@gmail.com>
2019-03-05 00:59:13 +0200

    Backend: create random game

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,45 +2,56 @@
 #[macro_use] extern crate rocket;
 
 extern crate rusqlite;
+extern crate rand;
+ 
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 use rusqlite::{Connection, Result, NO_PARAMS};
+mod boards;
+use boards::BOARDS;
 
 #[derive(Debug)]
 struct Game {
-    name: String
+    name: String,
+    state: String
 }
 
-#[get("/<game>")]
-fn index(game: String) -> Result<String> {
+#[get("/<game_name>")]
+fn index(game_name: String) -> Result<String> {
     let conn = Connection::open("games.db")?;
 
-    let mut stmt = conn.prepare("SELECT name from games WHERE name == $1")?;
+    let mut stmt = conn.prepare("SELECT name, state from games WHERE name == $1")?;
 
-    let games = stmt.query_map(&[&game], |row| {
+    let games: Vec<Game> = stmt.query_map(&[&game_name], |row| {
         Game {
             name: row.get(0),
+            state: row.get(1)
         }
-    })?;
+    })?.map(|row| row.unwrap()).collect();
 
-    if (games.count() > 0) {
-        return Ok(String::from("game exists!"));
+    if games.len() > 0 {
+        let game = &games[0];
+        return Ok(format!("game {} exists. Its state is {}", game_name, game.state));
     }
 
+    let mut rng = thread_rng();
     conn.execute(
-        "INSERT INTO games (name) values ($1)",
-        &[&game],
+        "INSERT INTO games (name, state) values ($1, $2)",
+        &[&game_name, &BOARDS.choose(&mut rng).unwrap().to_string()],
     )?;
 
     return Ok(String::from("Created game"));
 }
 
 fn init_db() -> Result<()> {
-    let mut conn = Connection::open("games.db")?;
+    let conn = Connection::open("games.db")?;
 
     conn.execute(
         "create table if not exists games (
              id integer primary key,
-             name text not null unique
+             name text not null unique,
+             state text not null unique
          )",
         NO_PARAMS,
     )?;
@@ -49,10 +60,13 @@ fn init_db() -> Result<()> {
 }
 
 fn main() {
-    init_db();
-
-    rocket::ignite()
-        .mount("/", routes![index])
-        .launch();
+    match init_db() {
+        Ok(()) => {
+            rocket::ignite()
+                .mount("/", routes![index])
+                .launch();
+        },
+        Err(_err) => println!("Failed to initialize db")
+    }
 }
 
