c3b6d2ce1908844074fbc8e3d5c1fc6109125d38
Alon Gal <alongalky@gmail.com>
2019-03-08 01:39:46 +0200

    Switched to in-memory state

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,72 +1,56 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
 
-extern crate rusqlite;
+use rocket::State;
+use rocket_contrib::json::Json;
+
 extern crate rand;
  
 use rand::thread_rng;
 use rand::seq::SliceRandom;
+use std::sync::Mutex;
+use std::collections::HashMap;
 
-use rusqlite::{Connection, Result, NO_PARAMS};
 mod boards;
 use boards::BOARDS;
 
-#[derive(Debug)]
+#[derive(Clone)]
+#[derive(Serialize, Deserialize)]
 struct Game {
     name: String,
     state: String
 }
 
+type ID = String;
+type MessageMap = Mutex<HashMap<ID, Game>>;
+
 #[get("/<game_name>")]
-fn index(game_name: String) -> Result<String> {
-    let conn = Connection::open("games.db")?;
-
-    let mut stmt = conn.prepare("SELECT name, state from games WHERE name == $1")?;
-
-    let games: Vec<Game> = stmt.query_map(&[&game_name], |row| {
-        Game {
-            name: row.get(0),
-            state: row.get(1)
-        }
-    })?.map(|row| row.unwrap()).collect();
-
-    if games.len() > 0 {
-        let game = &games[0];
-        return Ok(format!("game {} exists. Its state is {}", game_name, game.state));
+fn index(game_name: String, games: State<MessageMap>) -> Json<Game> {
+    let mut hashmap = games.lock().unwrap();
+    
+    if hashmap.contains_key(&game_name) {
+        return Json(hashmap.get(&game_name).unwrap().clone());
+    } else {
+        let mut rng = thread_rng();
+        let game = Game {
+            name: game_name.clone(),
+            state: BOARDS.choose(&mut rng).unwrap().to_string()
+        };
+        hashmap.insert(game_name.clone(), game.clone());
+        return Json(game);
     }
-
-    let mut rng = thread_rng();
-    conn.execute(
-        "INSERT INTO games (name, state) values ($1, $2)",
-        &[&game_name, &BOARDS.choose(&mut rng).unwrap().to_string()],
-    )?;
-
-    return Ok(String::from("Created game"));
 }
 
-fn init_db() -> Result<()> {
-    let conn = Connection::open("games.db")?;
-
-    conn.execute(
-        "create table if not exists games (
-             id integer primary key,
-             name text not null unique,
-             state text not null unique
-         )",
-        NO_PARAMS,
-    )?;
-
-    Ok(())
+#[catch(404)]
+fn not_found() -> &'static str {
+    "Not found"
 }
 
 fn main() {
-    match init_db() {
-        Ok(()) => {
-            rocket::ignite()
-                .mount("/", routes![index])
-                .launch();
-        },
-        Err(_err) => println!("Failed to initialize db")
-    }
+    rocket::ignite()
+        .mount("/", routes![index])
+        .register(catchers![not_found])
+        .manage(Mutex::new(HashMap::<ID, Game>::new()))
+        .launch();
 }
-
