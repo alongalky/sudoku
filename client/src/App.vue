e340222dc2bd5f542ac19fd50e473975e2f94763
Alon Gal <alongalky@gmail.com>
2019-03-10 01:40:50 +0200

    WIP: Shared state in server

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <Board :board="board" :sendDigit="sendDigit"/>
+    <Board :board="state.board" :sendDigit="sendDigit"/>
   </div>
 </template>
 
 <script>
+const socket = new WebSocket("ws://localhost:3012");
+socket.onmessage = function (event) {
+  const incomingState = JSON.parse(event.data).state;
+  if (incomingState) {
+    state.board = parseBoard(incomingState);
+  }
+}
+
 import Board from './components/Board.vue'
-import boards from './boards'
 
 const emptyCell = () => ({value: null})
 const givenCell = value => ({value, owner: 'given'})
@@ -19,23 +26,32 @@ const parseBoard = board => board
       return value === 0 ? emptyCell() : givenCell(value)
     }))
 
-const board = parseBoard(boards[Math.floor(Math.random()*boards.length)]);
+const emptyBoard = parseBoard('000000000000000000000000000000000000000000000000000000000000000000000000000000000');
+const state = {
+  board: emptyBoard
+};
 
 export const isDigitValid = ({board, irow, icol, digit}) => {
   const rowValues = board[irow].map(cell => cell.value)
-  const colValues = board.map((row, irow) => row[icol].value)
+  const colValues = board.map(row => row[icol].value)
 
   return !rowValues.some(val => val === digit) &&
     !colValues.some(val => val === digit)
 }
 
 const sendDigit = ({irow, icol}) => digit => {
-  board[irow][icol].value = digit
+  // state.board[irow][icol].value = digit
+  socket.send(JSON.stringify({
+    type: 'send_digit',
+    irow,
+    icol,
+    digit
+  }));
 }
 
 export default {
   name: 'app',
-  data: () => ({ board }),
+  data: () => ({ state }),
   components: {
     Board
   },
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,56 +1,121 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate serde_derive;
-
-use rocket::State;
-use rocket_contrib::json::Json;
-
 extern crate rand;
+extern crate serde_derive;
  
 use rand::thread_rng;
 use rand::seq::SliceRandom;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::collections::HashMap;
+use serde_json;
+use serde_json::json;
 
 mod boards;
 use boards::BOARDS;
 
 #[derive(Clone)]
-#[derive(Serialize, Deserialize)]
 struct Game {
     name: String,
     state: String
 }
 
-type ID = String;
-type MessageMap = Mutex<HashMap<ID, Game>>;
-
-#[get("/<game_name>")]
-fn index(game_name: String, games: State<MessageMap>) -> Json<Game> {
-    let mut hashmap = games.lock().unwrap();
-    
-    if hashmap.contains_key(&game_name) {
-        return Json(hashmap.get(&game_name).unwrap().clone());
-    } else {
-        let mut rng = thread_rng();
-        let game = Game {
-            name: game_name.clone(),
-            state: BOARDS.choose(&mut rng).unwrap().to_string()
-        };
-        hashmap.insert(game_name.clone(), game.clone());
-        return Json(game);
+impl Game {
+    fn update_digit(&mut self, icol: u8, irow: u8, digit: u8) {
+        let index: usize = (irow * 9 + icol) as usize;
+        self.state = self.state[..index].to_string() + &digit.to_string() + &self.state[(index+1)..].to_string();
     }
 }
 
-#[catch(404)]
-fn not_found() -> &'static str {
-    "Not found"
+type ID = String;
+type MessageMap = Arc<Mutex<HashMap<ID, Game>>>;
+
+use ws::listen;
+use ws::{Factory, Error, Handler, Handshake, CloseCode, Sender, Result, Message};
+use env_logger;
+const GAME_ID: &str = "abcde";
+
+struct Server {
+    out: Sender,
+    games: MessageMap
+}
+
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct SendDigitMessage {
+    icol: u8,
+    irow: u8,
+    digit: u8
+}
+
+impl Handler for Server {
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        let mut hashmap = self.games.lock().unwrap();
+        if !hashmap.contains_key(GAME_ID) {
+            println!("found key! {}", GAME_ID);
+            let mut rng = thread_rng();
+            let game = Game {
+                name: GAME_ID.to_string(),
+                state: BOARDS.choose(&mut rng).unwrap().to_string()
+            };
+            hashmap.insert(GAME_ID.to_string(), game.clone());
+        }
+        let game = hashmap.get(GAME_ID).unwrap();
+        println!("new connection");
+        
+        self.out.send(Message::text(json!({
+            "state": &game.state,
+            "gameName": &game.name
+        }).to_string()));
+        Ok(())
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("Client says {}", msg);
+
+        if let Message::Text(txt) = msg.clone() {
+            let v: serde_json::Value = serde_json::from_str(&txt).unwrap();
+            
+            if let typ = &v["type"] {
+                if typ == "send_digit" {
+                    let data: SendDigitMessage = serde_json::from_str(&txt).unwrap();
+
+                    let mut hashmap = self.games.lock().unwrap();
+                    let mut game = hashmap.get_mut(GAME_ID).unwrap();
+                    game.update_digit(data.icol, data.irow, data.digit);
+
+                    self.out.send(Message::text(json!({
+                        "state": &game.state,
+                        "gameName": &game.name
+                    }).to_string()));
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away   => println!("The client is leaving the site."),
+            CloseCode::Abnormal => println!(
+                "Closing handshake failed! Unable to obtain closing status from client."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+
+        // The connection is going down, so we need to decrement the count
+        // self.count.set(self.count.get() - 1)
+    }
+
+    fn on_error(&mut self, err: Error) {
+        println!("The server encountered an error: {:?}", err);
+    }
+
 }
 
 fn main() {
-    rocket::ignite()
-        .mount("/", routes![index])
-        .register(catchers![not_found])
-        .manage(Mutex::new(HashMap::<ID, Game>::new()))
-        .launch();
+    env_logger::init();
+    let mut map: MessageMap = Arc::new(Mutex::new(HashMap::<ID, Game>::new()));
+
+    listen("127.0.0.1:3012", |out| { Server { out: out, games: map.clone() } }).unwrap()
 }
